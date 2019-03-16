bb127d027764377e8777f92f53de565c5bfdc9dc
neverm1nd <reddigitalsw@gmail.com>
2019-03-16 07:11:12 +0300

    add file sync

--- bin.js
+++ bin.js
@@ -1,4 +1,33 @@
-const spark = require('.')
-let theFile = process.argv[2]
+'use strict'
 
-spark();
+const spark = require('.');
+const figlet = require('figlet');
+const fs = require('fs');
+
+const options = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+const prog = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
+
+function Line(length, char) {
+    let str = '';
+    for (let i = 0; i < length; i++) {  str += char; }
+    return str;
+};
+
+function Logo() {
+  figlet('SPARK', function(err, data) {
+    if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+    }
+    console.log('\x1b[31m%s\x1b[0m',data)
+    console.log(prog.name, 'v.'+prog.version, 'by '+prog.author, prog.license, '2019');
+    console.log(Line(process.stdout.columns, '_'));
+    console.log(`Watching for file changes on ${options.path}`);
+  });
+  return true;
+};
+
+Logo();
+spark(options);
--- spark.js
+++ spark.js
@@ -1,7 +1,43 @@
 'use strict'
 
 const fs = require('fs');
+const iconv = require('iconv-lite');
 
-module.exports = function() {
 
+module.exports = function(options) {
+  let bite_size = 256,
+      readbytes = 0,
+      file;
+
+  let lastLineFeed,
+      lineArray;
+  let valueArray = [];
+
+  fs.open(options.path, 'r', function(err, fd) { file = fd; readfile(); });
+
+  function readfile() {
+      let stats = fs.fstatSync(file);
+      if (stats.size < readbytes+1) {
+          setTimeout(readfile, 1000);
+      }
+      else {
+          fs.read(file, new Buffer(bite_size), 0, bite_size, readbytes, processor);
+      }
+  }
+
+
+function processor(err, bytecount, buff) {
+    lastLineFeed = iconv.decode(buff, options.encoding).toString().lastIndexOf('\n');
+    if (lastLineFeed > -1){
+        lineArray = iconv.decode(buff, options.encoding).toString().slice(0,lastLineFeed).split('\n');
+        for (let i=0 ; i<lineArray.length; i++){
+            valueArray.push(lineArray[i].split(','));
+        }
+        readbytes+=lastLineFeed+1;
+    } else {
+        readbytes+=bytecount;
+    }
+    console.log(valueArray[valueArray.length - 1][0]);
+    process.nextTick(readfile);
+  }
 }
